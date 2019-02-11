fd4b3eaaca01cf0869afc01200c722bb8a0586cb
landauleo <bambie7@mail.ru>
2019-02-11 23:53:14 +0300

    HW 1

--- Chatchatbox/ViewController.swift
+++ Chatchatbox/ViewController.swift
@@ -8,11 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum ViewControllerState: String {
+    case appearing, appeared, disappearing, disappeared, notLoaded, loaded, layouting, layouted
+}
 
+class ViewController: UIViewController {
+    
+
+    private var currentState = ViewControllerState.notLoaded
+    
+    private func viewControllerChanged(to state: ViewControllerState) {
+        #if DEBUG
+        print("View controller moved from state \(currentState) to state \(state)")
+        #endif
+        currentState = state
+    }
+    
+    private func loggingInfo(functionName: String) {
+        #if DEBUG
+        print(functionName)
+        #endif
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .loaded)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .appearing)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .appeared)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .layouting)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .layouted)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .disappearing)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        loggingInfo(functionName: "Function: \(#function))")
+        viewControllerChanged(to: .disappeared)
     }
 
 
